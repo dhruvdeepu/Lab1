457ac27be40462a8598b53b5cf4976de2e7f3591
dhruvdeepu <43298258+dhruvdeepu@users.noreply.github.com>
2018-10-13 14:52:48 -0700

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.du.flashyappy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+    }
+}
